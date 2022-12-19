60cdee0bdcce1ead5f540dd61c07df46f86c2b37
Lengalov <120996383+Lengalov@users.noreply.github.com>
2022-12-19 04:45:08 +0300

    Add files via upload

---
+++ 2) Выписать квадраты первых 100 чисел..pas
@@ -0,0 +1,8 @@
+﻿var i: integer;
+begin
+  i:=0;
+    repeat;
+      i:=i+1;
+      writeln(i, ' ',sqr(i), ' ', 'Квадрат')
+    until i>=100;
+end.
---
+++ 4)Найти сумму квадратов натуральных чисел от 4 до 37..pas
@@ -0,0 +1,10 @@
+﻿var i,s:integer;
+begin
+s:=0;
+i:=4;
+repeat
+s:=s+sqr(i);
+i:=i+1;
+writeln('Summ=',s);
+until i>=37;
+end.
